d974373f450f5feccb76e702c908f17f13a2b242
sam <skomesarook@protonmail.com>
2021-08-30 15:46:33 +1000

    feat: basic programs

---
+++ ContinuationLine.cbl
@@ -0,0 +1,6 @@
+           IDENTIFICATION DIVISION.
+           PROGRAM-ID. CONT.
+           PROCEDURE DIVISION.
+               DISPLAY "This is a super long line that I'd like to print 
+      -        "by breaking it onto a newline".
+           END PROGRAM CONT.
